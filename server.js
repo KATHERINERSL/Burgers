1f74faf60872d7891c6bab119c4d5fd8e3da4913
katherine Lundy <katherinersl@gmail.com>
2020-08-09 14:12:40 -0400

    cleaned up my files and folders

---
+++ server.js
@@ -0,0 +1,38 @@
+/*
+-------------------------------------------------------------------------------
+A main app server javascript for the burger app
+-------------------------------------------------------------------------------
+*/
+'use strict';
+
+var path = require('path');
+var PORT = process.env.PORT || 3000;
+
+// Load express
+var express = require('express');
+var exphbs  = require('express-handlebars');
+
+var app = express();
+
+// Load router module(s) and initialize
+var Router = require('./controllers/burgers_controller');
+var router = new Router(app);
+
+// Make use of the body-parsers
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set static directory reference path
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Start routers
+router.start();
+
+// Start the server to listen to the port
+app.listen(PORT, () => {
+  console.log('Server started listening on port ' + PORT);
+});
